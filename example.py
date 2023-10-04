b77ce9b7bf047f49410c6e362fdf49653377ff0d
Ubuntu <a.aitkaci@yahoo.fr>
2023-10-04 17:20:28 +0200

    h

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from cyberwatch_api import Cyberwatch_Pyhelper
 output = Cyberwatch_Pyhelper().request(
     method="get",
     endpoint="/api/v3/assets/servers/{id}",
-    params={'id' : 11453}
+    params={'id' : 12}
 )
 
 for res in output :
