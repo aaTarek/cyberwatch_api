359d4ab736a68bd370c3d51dfc5f26d8a634837d
Ubuntu <a.aitkaci@yahoo.fr>
2023-10-04 17:23:36 +0200

    Changed ID in example file

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from cyberwatch_api import Cyberwatch_Pyhelper
 output = Cyberwatch_Pyhelper().request(
     method="get",
     endpoint="/api/v3/assets/servers/{id}",
-    params={'id' : 12}
+    params={'id' : 1082}
 )
 
 for res in output :
